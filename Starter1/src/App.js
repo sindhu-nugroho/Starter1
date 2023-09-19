8cabb0afaab36fadd362b6b2924507fb8c622993
Sindhu Wijaya Mulyo Nugroho, S.Kom <88322797+sindhu-nugroho@users.noreply.github.com>
2023-09-19 21:48:26 +0700

    Add files via upload

---
+++ Starter1/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+          <span style={{color: 'white'}}>Follow Linkedin Mr Sindhu = Sindhu Nugroho S.Kom</span>
+    </div>
+  );
+}
+
+export default App;
